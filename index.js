c51754af61944e2b2455887466401b1520027d8f
Mary Luong <marysluong@gmail.com>
2017-09-18 05:43:13 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,32 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  
+}
+
+/*
+
+  2) Objects updateObjectWithKeyAndValue(object, key, value) returns an object with the orignal key value pairs and the ne
+     ReferenceError: updateObjectWithKeyAndValue is not defined
+      at Context.<anonymous> (test/index-test.js:12:14)
+  3) Objects updateObjectWithKeyAndValue(object, key, value) it does not modify the original object, but rather returns a
+     ReferenceError: updateObjectWithKeyAndValue is not defined
+      at Context.<anonymous> (test/index-test.js:21:7)
+  4) Objects destructivelyUpdateObjectWithKeyAndValue(object, key, value) updates `object` with the given `key` and `value
+d returns the entire updated object:
+     ReferenceError: destructivelyUpdateObjectWithKeyAndValue is not defined
+      at Context.<anonymous> (test/index-test.js:31:14)
+  5) Objects deleteFromObjectByKey(object, key) deletes `key` from a clone of object and returns the new object (it is non
+     ReferenceError: deleteFromObjectByKey is not defined
+      at Context.<anonymous> (test/index-test.js:46:20)
+  6) Objects deleteFromObjectByKey(object, key) does not modify the original object (it is non-destructive):
+     ReferenceError: deleteFromObjectByKey is not defined
+      at Context.<anonymous> (test/index-test.js:54:7)
+  7) Objects destructivelyDeleteFromObjectByKey(object, key) returns object without the delete key/value pair:
+     ReferenceError: destructivelyDeleteFromObjectByKey is not defined
+      at Context.<anonymous> (test/index-test.js:62:20)
+  8) Objects destructivelyDeleteFromObjectByKey(object, key) modifies the original object:
+     ReferenceError: destructivelyDeleteFromObjectByKey is not defined
+      at Context.<anonymous> (test/index-test.js:69:20)
+
+      */
