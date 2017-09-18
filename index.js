55e575d82b2072248848cd1166c2566ee975d8b8
Mary Luong <marysluong@gmail.com>
2017-09-18 05:53:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
+  object.key = value
   return object
 }
 
