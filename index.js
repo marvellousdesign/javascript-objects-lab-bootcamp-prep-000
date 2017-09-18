3aa1c2989fd24cd859be4a9ec5033fa1ba8254a9
Mary Luong <marysluong@gmail.com>
2017-09-18 06:01:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,23 +19,3 @@ function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key]
   return object
 }
-
-/*
-
-  4) Objects deleteFromObjectByKey(object, key) deletes `key` from a clone of object and returns the new object (it is non-destructive):
-     ReferenceError: deleteFromObjectByKey is not defined
-      at Context.<anonymous> (test/index-test.js:46:20)
-
-  5) Objects deleteFromObjectByKey(object, key) does not modify the original object (it is non-destructive):
-     ReferenceError: deleteFromObjectByKey is not defined
-      at Context.<anonymous> (test/index-test.js:54:7)
-
-  6) Objects destructivelyDeleteFromObjectByKey(object, key) returns object without the delete key/value pair:
-     ReferenceError: destructivelyDeleteFromObjectByKey is not defined
-      at Context.<anonymous> (test/index-test.js:62:20)
-
-  7) Objects destructivelyDeleteFromObjectByKey(object, key) modifies the original object:
-     ReferenceError: destructivelyDeleteFromObjectByKey is not defined
-      at Context.<anonymous> (test/index-test.js:69:20)
-
-      */
