ec250a9fc6eed540e53af2324b3dea22e6697fc4
Mary Luong <marysluong@gmail.com>
2017-09-18 05:50:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
 var recipes = {}
 
-// function updateObjectWithKeyAndValue(object, key, value) {
-//
-// }
+function updateObjectWithKeyAndValue(object, key, value) {
+  var newObj = Object.assign({}, object, {[key]: value})
+}
 
 /*
-c
-  2) Objects updateObjectWithKeyAndValue(object, key, value) returns an object with the orignal key value pairs and the ne
+1) Objects updateObjectWithKeyAndValue(object, key, value) returns an object with the orignal key value pairs and the new key value pair:
      ReferenceError: updateObjectWithKeyAndValue is not defined
       at Context.<anonymous> (test/index-test.js:12:14)
-  3) Objects updateObjectWithKeyAndValue(object, key, value) it does not modify the original object, but rather returns a
+      
+  2) Objects updateObjectWithKeyAndValue(object, key, value) it does not modify the original object, but rather returns a clone with the new data:
      ReferenceError: updateObjectWithKeyAndValue is not defined
       at Context.<anonymous> (test/index-test.js:21:7)
-  4) Objects destructivelyUpdateObjectWithKeyAndValue(object, key, value) updates `object` with the given `key` and `value
-d returns the entire updated object:
+      
+  3) Objects destructivelyUpdateObjectWithKeyAndValue(object, key, value) updates `object` with the given `key` and `value` (it is destructive) and
+returns the entire updated object:
      ReferenceError: destructivelyUpdateObjectWithKeyAndValue is not defined
       at Context.<anonymous> (test/index-test.js:31:14)
-  5) Objects deleteFromObjectByKey(object, key) deletes `key` from a clone of object and returns the new object (it is non
+      
+  4) Objects deleteFromObjectByKey(object, key) deletes `key` from a clone of object and returns the new object (it is non-destructive):
      ReferenceError: deleteFromObjectByKey is not defined
       at Context.<anonymous> (test/index-test.js:46:20)
-  6) Objects deleteFromObjectByKey(object, key) does not modify the original object (it is non-destructive):
+      
+  5) Objects deleteFromObjectByKey(object, key) does not modify the original object (it is non-destructive):
      ReferenceError: deleteFromObjectByKey is not defined
       at Context.<anonymous> (test/index-test.js:54:7)
-  7) Objects destructivelyDeleteFromObjectByKey(object, key) returns object without the delete key/value pair:
+      
+  6) Objects destructivelyDeleteFromObjectByKey(object, key) returns object without the delete key/value pair:
      ReferenceError: destructivelyDeleteFromObjectByKey is not defined
       at Context.<anonymous> (test/index-test.js:62:20)
-  8) Objects destructivelyDeleteFromObjectByKey(object, key) modifies the original object:
+
+  7) Objects destructivelyDeleteFromObjectByKey(object, key) modifies the original object:
      ReferenceError: destructivelyDeleteFromObjectByKey is not defined
       at Context.<anonymous> (test/index-test.js:69:20)
 
